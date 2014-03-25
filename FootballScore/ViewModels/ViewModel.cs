07439d851cc87536b128a1f60172bc5f952c7874
Romain Parage <rparage@talentsoft.com>
2014-03-25 13:38:39 +0100

    Add a theme and implement a simple binding

---
+++ FootballScore/ViewModels/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FootballScore.ViewModels
+{
+    public class ViewModel : BaseViewModel
+    {
+        public ViewModel()
+        {
+            Text = "lola";
+        }
+
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged("Text"); }
+        }
+    }
+}
---
+++ FootballScore/Views/View.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace FootballScore.Views
+{
+    using FootballScore.ViewModels;
+
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class View : Window
+    {
+        public View()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+    }
+}
